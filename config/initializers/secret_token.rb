e0ea3228e8315602d7c73d1d37e73747768bb84d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-07-19 10:26:58 +0900

    m
    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GAR1::Application.config.secret_token = '872569a28097faf05b91a7d27a3f8c3d448231fe425f6c185f7352f53cab819bcae2d590180e5a1c77d274f53678201e29989237e6eadfedaffaefd0f803b95b'
