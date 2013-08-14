35239ee37a25f768ad9700b183cc747cf0110654
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-08-14 09:17:58 +0900

    B3
    v-1.0a
    ---
    1. Time label => "@time_label_time"

--- app/controllers/sandbox_controller.rb
+++ app/controllers/sandbox_controller.rb
@@ -11,6 +11,7 @@ class SandboxController < ApplicationController
   def sub3
       
       @time_label = Time.new.strftime("%d/%m/%Y %H:%M:%S")
+      @time_label_time = Time.new.strftime("%H:%M:%S")
       
   end
   
