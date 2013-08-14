f6d35036580be4714cb824bab5cd80647c95899f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-08-14 09:27:53 +0900

    B3
    v-1.0b
    ---
    1. Time zone => 'Tokyo'

--- app/controllers/sandbox_controller.rb
+++ app/controllers/sandbox_controller.rb
@@ -10,7 +10,11 @@ class SandboxController < ApplicationController
 
   def sub3
       
+      Time.zone = 'Tokyo'
+      
+      #REF Time.new http://www.tutorialspoint.com/ruby/ruby_date_time.htm
       @time_label = Time.new.strftime("%d/%m/%Y %H:%M:%S")
+      # @time_label = Time.local.strftime("%d/%m/%Y %H:%M:%S")
       @time_label_time = Time.new.strftime("%H:%M:%S")
       
   end
